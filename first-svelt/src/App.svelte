30c0b9a44047d01ae8be67f324661a8acd14d7dc
RootOfMinus1ATU <s00240122@atu.ie>
2023-12-04 14:25:46 +0000

    first

---
+++ first-svelt/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  let mouse = { x: 0, y: 0 }
+
+  function handleMouseMove(e) {
+    console.log(e, mouse)
+    mouse.x = e.clientX
+    mouse.y = e.clientY
+  }
+  
+</script>
+
+<main style="height: 100vh;">
+  <div on:mousemove={handleMouseMove}>
+    The mouse position is ({mouse.x}, {mouse.y}) 
+  </div>
+
+</main>
+
+<style>
+  div {
+    height: 100%;
+    width: 100%;
+  }
+</style>
