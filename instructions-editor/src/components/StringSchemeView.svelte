1b99204e09cb48056fde3bfcdb760e65a5064284
RootOfMinus1ATU <s00240122@atu.ie>
2024-12-04 16:59:05 +0000

    Update StringSchemeView.svelte

--- instructions-editor/src/components/StringSchemeView.svelte
+++ instructions-editor/src/components/StringSchemeView.svelte
@@ -8,6 +8,9 @@
         scheme.path = 'div > p'
     }
 
+
+    // TODO: figure out a way to convey that a view is "WIP", for example a string needs to have a path, but it starts off with an empty path, this is not ideal, better to have a `finalized` field or some state transitions
+
     let content = $derived(`${scheme.path} CONTENT`)
 
     $effect(() => {
